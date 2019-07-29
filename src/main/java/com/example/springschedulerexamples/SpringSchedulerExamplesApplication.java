d6d1ef44ba53628026f1c2bfa770b387594fe9d5
Peter Stritzinger <der.stritzinger@gmail.com>
2019-07-29 14:38:14 +0200

    initial load

---
+++ src/main/java/com/example/springschedulerexamples/SpringSchedulerExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springschedulerexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedulerExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedulerExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springschedulerexamples/SpringSchedulerExamplesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springschedulerexamples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringSchedulerExamplesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
